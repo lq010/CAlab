b8bc3867fea7df914eb8d76e8beb448399302300
simoiamma <simoiamma@hotmail.it>
2017-11-08 14:06:01 +0100

    Update 08/11/2017 13:37

--- LAB2/ex2.asm
+++ LAB2/ex2.asm
@@ -13,88 +13,69 @@ FIRST_ROW  DB MAXDIM DUP(?)
 SECOND_ROW DB MAXDIM DUP(?)
 THIRD_ROW  DB MAXDIM DUP(?)
 FOURTH_ROW DB MAXDIM DUP(?)
-var DW 0
+temp DB 4
 
 .CODE
 .STARTUP
 
 ex1_readFromKey:
-MOV CX,MAXDIM
-XOR DI,DI
+;init counter
+MOV CX,4
 MOV AH,1 ;instruct 21H ISR to read and save key value into AL
-
-firstRow: 
-INT 21H ;wait interrupt (press key)
-MOV FIRST_ROW[DI],AL ;the char (1byte) is saved into AL (21H ISR)
-CMP AL, RETURN ;if return character jump to second row
-JZ secondRowInit ;jump if are equal
-INC DI
-LOOP firstRow
-
-secondRowInit: ;reset variable
-MOV CX,MAXDIM
-XOR DI,DI
-secondRow:
-INT 21H
-MOV SECOND_ROW[DI],AL
-CMP AL, RETURN
-JZ thirdRowInit
-INC DI
-LOOP secondRow
-
-thirdRowInit:
-MOV CX,MAXDIM
-XOR DI,DI
-thirdRow:
-INT 21H
-MOV THIRD_ROW[DI],AL
-CMP AL, RETURN
-JZ fourthRowInit
-INC DI
-LOOP thirdRow
-
-fourthRowInit:
-MOV CX,MAXDIM
-XOR DI,DI
-fourthRow:
-INT 21H
-MOV FOURTH_ROW[DI],AL
-CMP AL, RETURN
-JZ ex2_countChar
-INC DI
-LOOP fourthRow
-
-ex2_countChar: 
-MOV CX,MAXDIM
-XOR DI,DI
 XOR BX,BX
-MOV BL,offsetFIRST_ROW
-nextChar:
-MOV AL, FIRST_ROW[DI] ;select char
-;CMP AL, RETURN ;check end
-CALL countChar
-INC DI
-LOOP nextChar
 
+read:
+ PUSH CX
+ MOV CX,MAXDIM ;init internal cycle
+ XOR DI,DI
+ readRow: 
+  INT 21H ;wait interrupt (press key)
+  CMP AL, RETURN ;if return character jump to check20
+  JZ check20 ;control if are minimum 20
+  MOV FIRST_ROW[BX][DI],AL ;the char (1byte) is saved into AL (21H ISR) (skipped if \n)
+  INC DI  
+ LOOP readRow
+ exitRow:
+ POP CX
+ ADD BX,MAXDIM
+LOOP read
 
+check20:
+CMP DI,20 ;if there are 20 chars continue to next row
+JB readRow ;if <20 continue input
+JMP exitRow
+
+;ex2_countChar: 
+; MOV CX,MAXDIM
+; XOR DI,DI
+; XOR BX,BX
+; MOV BL,offset FIRST_ROW
+; nextChar:
+; MOV AL, FIRST_ROW[DI] ;select char
+; CMP AL, RETURN ;check end
+; CALL countChar
+; INC DI
+; LOOP nextChar
+
+exit_label:
 .EXIT
 
 ;is char already 
 
 ;how many times each char is present in a row?
-countChar PROC
-PUSH CX
-PUSH DI
-XOR SI,SI ;time counter
-countLoop:    
-CMP AL,BX[DI]
-JNZ endLoop ;if not equal does not increment SI
-INC SI
-endLoop:
-LOOP countLoop
-MOV var,SI
-POP DI
-POP CX
-countChar ENDP
+; countChar PROC
+; PUSH CX
+; PUSH DI
+; XOR SI,SI ;time counter
+; countLoop:    
+; CMP AL,BX[DI]
+; JNZ endLoop ;if not equal does not increment SI
+; INC SI
+; endLoop:
+; LOOP countLoop
+; MOV var,SI
+; POP DI
+; POP CX
+; countChar ENDP
 
 END
