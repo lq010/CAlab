7f374f298c4617c16b06a619e6d2935409f54ba2
simoiamma <simoiamma@hotmail.it>
2017-11-17 12:59:36 +0100

    Update 17/11/2017 12:30

--- 8086/LAB2/ex2.asm
+++ 8086/LAB2/ex2.asm
@@ -2,7 +2,8 @@
 ; Lab n.2
 ; Simone Iammarino 06/11/2017
 
-MAXDIM EQU 50
+MAXDIM EQU 10
+MINDIM EQU 5
 RETURN EQU 0dh ;binary value of return character \n
 
 .MODEL small
@@ -13,103 +14,90 @@ FIRST_ROW  DB MAXDIM DUP(?)
 SECOND_ROW DB MAXDIM DUP(?)
 THIRD_ROW  DB MAXDIM DUP(?)
 FOURTH_ROW DB MAXDIM DUP(?)
-temp DB 4
-
+CKDCHR DB 26 DUP(0)
 .CODE
 .STARTUP
 
 ex1_readFromKey:
-;init read cycle
-MOV CX,4
+MOV CX,4 ;init row cycle
 MOV AH,1 ;instruct 21H ISR to read and save key value into AL
 XOR BX,BX
-read:
- PUSH CX ;do a push of counter cycle each nested cycle
- ;init readRow cycle
- MOV CX,MAXDIM 
- XOR DI,DI
- readRow: 
-  INT 21H ;wait interrupt (press key)
-  ;A: if \n jump to check20
-  CMP AL, RETURN
-  JZ check20
-  ;else save the character
-    MOV FIRST_ROW[BX][DI],AL ;the char (1byte) is saved into AL (21H ISR) (skipped if \n)
-                             ;the row are saved sequentially in the memory so the starting address can be FIRST_ROW
-    INC DI
-    JMP skipCheck20 ;(must terminate skipping the other condition code)
-  check20: ; A: code
-    CMP DI,20
-    ;if number of char inserted < 20 continue input row (without update CX or DI)
-    JB readRow
+;for(CX=4,CX=0,CX--)
+row_loop:
+PUSH CX ;push counter each nested cycle
+MOV CX,MAXDIM ;init char cycle 
+XOR DI,DI
+  ;for(CX=MAXDIM,CX=0,CX--)
+  char_loop: 
+    INT 21H ;wait interrupt (press key)
+    ;if char = \n check exit condition
+      CMP AL, RETURN
+      JE check20
+    ;else save the character
+      MOV FIRST_ROW[BX][DI],AL ;rows saved sequentially in memory, we can reach all starting from FIRST_ROW
+      INC DI
+      JMP skipCheck20 ;(must terminate skipping the other condition code)
+    check20:
+    ;if number of chars < 20 continue input (without update CX or DI)
+      CMP DI,MINDIM
+      JB char_loop
     ;else jump to next row 
-    JMP exitRow
-  skipCheck20:  
- LOOP readRow
- exitRow:
- POP CX
- ADD BX,MAXDIM ;offset for next row
-LOOP read
+      JMP new_row
+    skipCheck20:  
+  LOOP char_loop
+  ;end for (char_loop)
+new_row:
+POP CX
+ADD BX,MAXDIM ;offset for next row
+LOOP row_loop
+;end for (row_loop)
 
 ex2_countChar:
-;init nextRow cycle
 MOV CX,4
 XOR BX,BX
-nextRow: ;foreach row
- PUSH CX
- ;init nextChar cycle
- MOV CX,MAXDIM 
- XOR DI,DI
- XOR AX,AX
- nextChar: ;foreach char
+XOR SI,SI
+;for(CX=4,CX=0,CX--)
+row_loop_2:
   PUSH CX
-  PUSH DI ;position of current char
-  MOV AL,FIRST_ROW[BX][DI] ;select char
-  MOV CX,MAXDIM 
-   scan:  
-    CMP AL,FIRST_ROW[BX][CX] ;compare char
-    ;if not equal try next element of array
-    JNE notFind
-    ;else increment counter
-    INC DI ;number of time founded
-    notFind:
-   LOOP scan 
-     
- LOOP readRow
- exitRow:
- POP CX
- ADD BX,MAXDIM
-LOOP read
- 
-; MOV CX,MAXDIM
-; XOR DI,DI
-; XOR BX,BX
-; MOV BL,offset FIRST_ROW
-; nextChar:
-; MOV AL, FIRST_ROW[DI] ;select char
-; CMP AL, RETURN ;check end
-; CALL countChar
-; INC DI
-; LOOP nextChar
+  MOV CX,MAXDIM
+  ;for(CX=MAXDIM,CX=0,CX--)
+  char_loop_2:
+    MOV AL, FIRST_ROW[BX][SI]
+    CALL alreadyChecked
+    ;if BX=1: char already checked, skip to next char
+    ;else: count the time that appares in the row
+    INC SI
+  LOOP char_loop_2
+  ;end for (char_loop_2)
+  XOR SI,SI
+  ADD BX,MAXDIM
+LOOP row_loop_2
+;end for (row_loop_2)
 
 .EXIT
 
-;is char already 
+;checks if the character is already counted in that row
+;character passed into AL
+;result writed into: AH=0 -> counted, AH=0 -> not counted
+alreadyChk PROC
+PUSH SI
+XOR AH,AH
+XOR SI,SI
 
-;how many times each char is present in a row?
-; countChar PROC
-; PUSH CX
-; PUSH DI
-; XOR SI,SI ;time counter
-; countLoop:    
-; CMP AL,BX[DI]
-; JNZ endLoop ;if not equal does not increment SI
-; INC SI
-; endLoop:
-; LOOP countLoop
-; MOV var,SI
-; POP DI
-; POP CX
-; countChar ENDP
+;if CHKCHR empty set AH=0 and return
+  CMP CHKCHR[0],0
+  JE skip_empty
+;else
+  ;while EOS CHKCHR (End Of String) 
+  check_loop:
+    ;if present: AH=1
+      CMP AL,CHKCHR[SI]
+    ;else: AH=0
+    ;end wile (check_loop)
+skip_empty:
+MOV AH,0  
+POP SI
+RET
+alreadyChecked ENDP
 
 END
