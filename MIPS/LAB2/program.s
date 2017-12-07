d1211afe2e2f0079bd20fe29b44a974933452d05
simoiamma <simoiamma@hotmail.it>
2017-12-07 17:26:22 +0100

    Update MIPS LAB2 finished

--- MIPS/LAB2/program.s
+++ MIPS/LAB2/program.s
@@ -1,29 +1,36 @@
 .data
 
-X: .byte 19
-
+X: .byte 1,2,3,4,5,6,7,8,9,10 ;the even parity of each byte will be 1 and 0 alternatively
+							  ;expected results: 128(1000 0000)+X[1] when #ones odd
+							  ;				     X[i] when #ones even
+							  ;(expressed in hexadecimal in winMips)
 .code
 
-daddui r1,r0,0 ;cycle counter
-daddui r4,r0,0 ;init 1's counter
-daddui r10,r0,7 ;cycle limit
-daddui r11,r11,1 ;value 1 register
+daddui r10,r0,7	 ;"scan byte" cycle limit
+daddui r11,r0,10 ;"scan X" cycle limit 
 
-daddui r5,r0,0 ;reset parity register
-lb r2,X(r1)
-parity_loop:
-	dsll r2,r2,1
-	daddu r3,r0,r2 ;save into r3 for not modify shifted value after applying the mask
-	andi r3,r3,256 ;mask to select only shifted bit (9th bit)
-	daddu r4,r4,r3 ;add using 15downto8 bits, at the end shift to obtain correct decimal value
-	bne r1,r10,parity_loop
-;check parity with r4
-;if even (LSB=0) parity = 0
-dsrlv r4,8 ;shift counter to bit 8downto0
-andi r4,r4,1 ;check LSB if 1 or 0
-movn r5,r11,r4 ;if odd (r4 LSB=1) then parity = 1 (r5=1)
-;save parity bit into X MSB
-ori r2,r2,128
-sb r2,X(r1)
+;while (r1<10) : scans X vector 
+scan_x: 
+	daddui r5,r0,0 ;reset "ones counter"
+	daddui r4,r0,0 ;reset "scan byte" cycle counter 
+
+	;while(r1<7) : scans X[r1] byte and compute parity bit
+	lb r2,X(r1)
+	scan_byte:
+		andi r3,r2,1 	;save LSB into r3
+		daddu r5,r3,r0 	;update "ones counter"
+		daddui r4,r4,1 	;update cycle counter
+	bne r4,r10,scan_byte
+	;end while(scan_byte)
+
+	;EVEN PARITY: if #ones odd (LSB=1) -> parity_bit=1
+	andi r5,r5,1 ;select LSB
+	dsll r5,r5,7 ;move to MSB
+	or r2,r5,r2  ;save parity bit into X[r1] MSB
+	sb r2,X(r1)  ;store result
+
+	daddui r1,r1,1
+bne r1,r11,scan_x
+;end while(scan_x)
 
 HALT
