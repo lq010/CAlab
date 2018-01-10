b0d5c891725c7779534649742e55601320078ad4
simoiamma <simoiamma@hotmail.it>
2018-01-10 22:57:20 +0100

    update mips scheduling example

--- MIPS/exam/example_scheduling.s
+++ MIPS/exam/example_scheduling.s
@@ -25,11 +25,11 @@ loop:
 l.d f0,0(r1)    ;FDEMW 1  
 daddi r1,r1,-8  ; FDEMW 2
 add.d f4,f0,f2  ;  FDsEMW 4 (1 cycle delay after l.d memory stage)
-s.d f4,8(r1)    ;   FDEssMW 6 (2 cycle delay after add.d execute stage)
-bne,r1,r2,loop  ;    FD--- 0
-;l.d f0,0(r1)   ;     FDsEMW 1 (no more delay next loop cycles) 
+s.d f4,8(r1)    ;   FsDEsMW 6 (2 cycle delay after add.d execute stage)
+bne,r1,r2,loop  ;    FsD--- 0
+;l.d f0,0(r1)   ;     sFDsEMW 2 (the second loop take 7 clock cycle to execute
  
-;total cycles: 6*1000 = 6000
+;total cycles: 6*1 + 7*999 = 6999
 
 ; 3) LOOP UNROLLING: only 3 instruction out of 5 are usefull for computation, the others are used for the loop
 ; because 1000 is multiple of 4 we can unroll the loop by 4, obtaining 1000/4 = 250 iterations
@@ -39,7 +39,7 @@ add.d f4,f0,f2  ;  FDssEMW 4
 s.d f4,0(r1)    ;   FssDEsMW 6
 daddi r1,r1,-8  ;    ssFDsEMW 7
 l.d f0,0(r1)    ;     ssFsDEMW 8 (stall for r1!)
-add.d f4,f0,f2  ;      ssFsDssEMW 11
+add.d f4,f0,f2  ;      sssFDssEMW 11
 s.d f4,0(r1)    ;       sssFssDEsMW 13
 daddi r1,r1,-8  ;        sssssFDsEMW 14
 l.d f0,0(r1)    ;         sssssFsDEMW 15
@@ -54,7 +54,7 @@ bne,r1,r2,loop  ;                 sssssssssssFssD--- 30 (cause a delay of 2 cloc
 ;l.d f0,0(r1)   ;                  sssssssssssssFDEMW (OK, no hazards or dependencies with previous loop, the loop start in the same way)
 
 ;total cycles: 30*250 = 7500  
-;(>6000 of the compressed loop, because daddi produces data dependencies and cannot be moved like the previous scheduling due to data dependencies that will occour with the subsequent s.d instruction)
+;(>6999 of the compressed loop, because daddi instruction cannot be moved like the previous scheduling due to data dependencies that will occour w.r.t the subsequent s.d instruction)
 
 ;4) OFFSET PRE-CALCULATION: instead of increase each time r1, we can pre calculate the offsets in this way:
 loop:
@@ -65,11 +65,33 @@ l.d f0,-8(r1)   ;    ssFDsEMW 7
 add.d f4,f0,f2  ;     ssFsDssEMW 10
 s.d f4,-8(r1)   ;      sssFssDEsMW 12
 l.d f0,-16(r1)  ;       sssssFDsEMW 13
-add.d f4,f0,f2  ;        sssssFsD
-s.d f4,-16(r1)  ;
-l.d f0,-24(r1)  ;
-add.d f4,f0,f2  ;
-s.d f4,-24(r1)  ;
-daddi r1,r1,-32 ;
-bne,r1,r2,loop  ;
+add.d f4,f0,f2  ;        sssssFsDssEMW 16
+s.d f4,-16(r1)  ;         ssssssFssDEsMW 18
+l.d f0,-24(r1)  ;          ssssssssFDsEMW 19
+add.d f4,f0,f2  ;           ssssssssFsDssEMW 21
+s.d f4,-24(r1)  ;            sssssssssFssDEsMW 23
+daddi r1,r1,-32 ;             sssssssssssFDsEMW 24
+bne,r1,r2,loop  ;              sssssssssssFssD--- 26
+;l.d f0,0(r1)   ;               sssssssssssssFDEMW (OK, the same as previous loop)
+
+;total cycles: 26*250 = 6500 (500 cycle less than non optimized scheduling)
+
+;5) REGISTER RENAMING: recheduling doing all together the l.d at the beginning and the s.d at the end relying on different registers, parallelizing the operations:
+loop:
+l.d f0,0(r1)    ; FDEMW 1
+l.d f6,-8(r1)   ;  FDEMW 2
+l.d f10,-16(r1) ;   FDEMW 3
+l.d f14,-24(r1) ;    FDEMW 4
+add.d f4,f0,f2  ;     FDEMW 5
+add.d f8,f6,f2  ;      FDEMW 6
+add.d f12,f10,f2;       FDEMW 7
+add.d f16,f14,f2;        FDEMW 8 
+s.d f4,-0(r1)   ;         FDEMW 9
+s.d f8,-8(r1)   ;          FDEMW 10
+daddi r1,r1,-32 ;           FDEMW 11
+s.d f12,-16(r1) ;            FDEMW 12
+s.d f16,-24(r1) ;             FDEMW 13
+bne,r1,r2,loop  ;              FD--- 14
 ;l.d f0,0(r1)   ;
+
+;total cycles: 14*250 = 3500 (best performance, technique limited by the maximum number of registers)
