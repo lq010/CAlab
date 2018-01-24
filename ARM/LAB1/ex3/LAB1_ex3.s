c1fba79148b5ef4f37552b9433be6c53dea02a64
simoiamma <simoiamma@hotmail.it>
2018-01-24 23:34:34 +0100

    update ARM lab

--- ARM/LAB1/ex3/LAB1_ex3.s
+++ ARM/LAB1/ex3/LAB1_ex3.s
@@ -9,7 +9,7 @@ min_value SPACE size_32
 	AREA MYCODE, CODE, READONLY
 	ENTRY
 	MOV R0,#0 ;(otherwise give the error "Entry point does not point to an instruction")	
-sequence DCD -2,-1,3,3,5,6,7,8
+sequence DCD 8,7,6,5,50,4,-1,-5
 	
 	;check increasing
 	MOV R1,#0x80000000 ;lowest possibl value at start (signed)
@@ -17,7 +17,7 @@ sequence DCD -2,-1,3,3,5,6,7,8
 increasing_loop
 	LDR R2,[R0],#4 ;load value (post-addressing mode)
 	CMP R2,R1
-	BLT decreasing_loop ;exit directly if found R2 < R1
+	BLT decreasing ;exit directly if found R2 < R1
 	MOVNE R1,R2 ;update previous value (if not equal)
 	ADD R4,R4,#1
 	CMP R4,#8
@@ -42,13 +42,14 @@ mean_loop
 	B end_programm
 
 	;check decreasing
+decreasing
 	MOV R4,#0
 	MOV R1,#0x7fffffff ;highest possible value at start (signed)
 	LDR R0,=sequence ;pointer to sequence
 decreasing_loop
 	LDR R2,[R0],#4 ;load value (post-addressing mode)
 	CMP R2,R1
-	BGE monotonic ;exit directly if found R2 > R1
+	BGT monotonic ;exit directly if found R2 > R1
 	MOVNE R1,R2 ;update previous value (if not equal)
 	ADD R4,R4,#1
 	CMP R4,#8
@@ -60,19 +61,22 @@ decreasing_loop
 	MOV R2,#0
 	MOV R4,#0 ;max absolute difference temp register
 lad_loop
-	MOV R1,[R0]
-  MOV R2,[R0],#4
+  LDR R1,[R0],#4
+  LDR R2,[R0]
   SUB R1,R2,R1
-	;invert if negative (absolute value) using Bit Clear
+  ;invert if negative (absolute value) using Bit Clear
   CMP R1,#0
   BICLT R1,R5,R1 ;R5=0
   ADD R1,R1,#1
   CMP R1,R4
   MOVGT R4,R1
   ADD R6,R6,#1
-  CMP R6,#8
+  CMP R6,#7 ;one less than array lenght in this case (otherwise go after array the last element)
   BNE lad_loop
-	B end_programm
+
+  LDR R0,=max_differ
+  STR R4,[R0]
+  B end_programm
 
 	;monotonic sequence, find min and max value
 monotonic
@@ -82,7 +86,7 @@ monotonic
   MOV R3,#0 ;temp for minimum
   MOV R4,#0 ;counter register
 min_max_loop
-  MOV RI,[R0],#4
+  LDR R1,[R0],#4
   CMP R1,R2
   MOVGT R2,R1 ;update max
   CMP R1,R3
@@ -90,7 +94,13 @@ min_max_loop
   ADD R4,R4,#1
   CMP R4,#8
   BNE min_max_loop
-  
-  end_programm
+ 
+  ;store result in memory
+  LDR R0,=max_value
+  STR R2,[R0]
+  LDR R0,=min_value
+  STR R3,[R0]
+
+end_programm
 	 
-	END
+  END
